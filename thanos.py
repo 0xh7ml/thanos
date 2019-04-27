b8f9bde2f207cf72ffdb852f7b667ba7f4db6244
Md Saikat <42938253+D4rk-h7ml@users.noreply.github.com>
2019-04-27 15:43:10 +0600

    Add files via upload

---
+++ thanos.py
@@ -0,0 +1,18 @@
+from sys import platform
+import os
+if platform == "win32":
+    print("This System Technology detected as Windows")
+    cd =os.chdir("C:/Users/Saikat/Desktop/test/")
+    path = os.getcwd()
+    print("Iron man founded Thanos on "+path)
+    for i in range(1,100000):
+        os.mkdir("Thanos" + str(i))
+    print("Killed Thanos Successfully")
+elif platform == "linux":
+    print("This system is Linux")
+    cd =os.chdir("/Desktop/")
+    path = os.getcwd()
+    print("Iron man founded Thanos on "+path)
+    for i in range(1,100000):
+        os.mkdir("Thanos" + str(i))
+    print("Killed Thanos Successfully")
